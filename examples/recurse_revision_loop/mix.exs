788513360754f7d0d453e3d41ea30c5aad5619d1
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2026-03-15 12:26:19 +0530

    docs: add recurse_revision_loop example

---
+++ examples/recurse_revision_loop/mix.exs
@@ -0,0 +1,26 @@
+defmodule RecurseRevisionLoop.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :recurse_revision_loop,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {RecurseRevisionLoop.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:ash_durable_reactor, path: "../.."}
+    ]
+  end
+end
